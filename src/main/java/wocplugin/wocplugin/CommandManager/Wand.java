af587a383421e5f8745eb1e596420e97e7cd2beb
Lachlan Jowett <lochyjowett1@gmail.com>
2022-09-09 09:20:20 +1000

    Added a dagger
    TODO: rename the wand command

--- src/main/java/wocplugin/wocplugin/CommandManager/Wand.java
+++ src/main/java/wocplugin/wocplugin/CommandManager/Wand.java
@@ -4,6 +4,7 @@ import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.Player;
+import wocplugin.wocplugin.Items.Items.Dagger;
 import wocplugin.wocplugin.Items.Items.ShortBow;
 import wocplugin.wocplugin.Items.Items.WarpWand;
 
@@ -21,6 +22,8 @@ public class Wand implements CommandExecutor {
                 player.getInventory().addItem(WarpWand.give());
             } else if (args[0].equalsIgnoreCase("bow")) {
                 player.getInventory().addItem(ShortBow.give());
+            } else if (args[0].equalsIgnoreCase("dagger")) {
+                player.getInventory().addItem(Dagger.give());
             }
         }
         return true;
